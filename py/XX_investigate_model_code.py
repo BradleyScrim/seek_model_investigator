f535b305e67ec825ae7717a29e1d01441a2c471d
winterb <winterb@uni-greifswald.de>
2020-10-16 18:05:56 +0200

    get info about version+level from attributes

--- py/XX_investigate_model_code.py
+++ py/XX_investigate_model_code.py
@@ -8,32 +8,42 @@ directory_to_read = r'data/'
 file_to_save = r'data/namespaces.csv'
 
 
-def get_namespace(xml_file: str) -> List:
+def get_namespace(xml_file: str) -> Dict:
     try:
         tree: ElementTree = ET.parse(xml_file)
     except ET.ParseError:
-        return []
+        return {}
     root: ElementTree.Element = tree.getroot()
 
     ns = {'sbml': 'http://www.sbml.org/sbml/*', }
 
     child: ElementTree.Element
-    base_elements: List = []
+    base_elements: Dict = {}
     for child in root.findall('.'):  # '//*[local-name()="model"]'):#'sbml:model', ns):
         # print("child", child)
         # print("tag", child.tag)
         # print("attrib", child.attrib)
         # print("text", child.text)
+
+        data: Dict = {}
         try:
-            namespace = re.search('.*{(.*)}.*', str(child)).group(1)
+            data['namespace'] = re.search('.*{(.*)}.*', str(child)).group(1)
         except AttributeError:
             continue
-        base_elements.append(namespace)
+        try:
+            data['version'] = child.attrib['version']
+        except KeyError:
+            pass
+        try:
+            data['level'] = child.attrib['level']
+        except KeyError:
+            pass
+        base_elements = data
 
     return base_elements
 
 
-collector: Dict[str, List] = {}
+collector: Dict[str, Dict] = {}
 
 entry: os.DirEntry
 for entry in os.scandir(directory_to_read):
@@ -43,9 +53,19 @@ for entry in os.scandir(directory_to_read):
         print("skip", entry.path)
 
 with open(file_to_save, "w") as file:
-    file.write("file name; count namespaces; namespaces\n")
+    file.write("file name;namespaces;attrib level;attrib version\n")
 
     key: str
-    value: List
+    value: Dict
     for key, value in collector.items():
-        file.write("{0};{1};{2}\n".format(key, len(value), value))
+        n = ""
+        v = ""
+        l = ""
+        if 'namespace' in value:
+            n = value['namespace']
+        if 'version' in value:
+            v = value['version']
+        if 'level' in value:
+            l = value['level']
+
+        file.write("{0};{1};{2};{3}\n".format(key, n, l, v))
